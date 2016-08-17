19913490233d16417aa70166795b26caefa3ff3b
Pengyao Chen <pengyaoc@gmail.com>
2016-08-16 23:45:25 -0700

    Fix home page

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -7,11 +7,7 @@ import hello.views
 import newsletter.views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', "newsletter.views.home", name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', newsletter.views.home, name='home'),
     url(r'^home', newsletter.views.home, name='home'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
