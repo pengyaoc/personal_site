fc3211e30401c3876870de702e8e383961d9ae9c
Pengyao Chen <pengyaoc@gmail.com>
2016-08-24 22:58:54 -0700

    [Payment] Add payment test

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -9,6 +9,7 @@ import newsletter.views
 urlpatterns = patterns('',
     url(r'^$', newsletter.views.home, name='home'),
     url(r'^home', newsletter.views.home, name='home'),
+    url(r'^payment', newsletter.views.payment, name='payment'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,7 +1,27 @@
+import stripe
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    #return HttpResponse('Home')
     return render(request, "newsletter/about.html", {})
+
+def payment(request):
+    stripe.api_key = "sk_test_a53eN187P0pYwmu8SXPV6QSZ"
+
+    # Get the credit card details submitted by the form
+    token = request.POST.get('stripeToken')
+
+    if token:
+        # Create a charge: this will charge the user's card
+        try:
+          charge = stripe.Charge.create(
+              amount=1000, # Amount in cents
+              currency="usd",
+              source=token,
+              description="Example charge"
+          )
+        except stripe.error.CardError as e:
+          # The card has been declined
+          pass
+    print("sdfsfd")
+    return render(request, "newsletter/contact.html", {})
